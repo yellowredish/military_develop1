e76add658bd08da3bfe50c061a4931293769fb70
yellowredish <68762850+yellowredish@users.noreply.github.com>
2026-05-30 06:34:30 +0000

    상점창 기능 추가

--- clicker.js
+++ clicker.js
@@ -9,3 +9,12 @@ function update() {
     document.querySelector("#countNum").value = gold;
     
 }
+
+function shop(){
+    document.getElementById("shopWindow").style.display = "block";
+
+}
+
+function closeShop(){
+    document.getElementById("shopWindow").style.display = "none";
+}
